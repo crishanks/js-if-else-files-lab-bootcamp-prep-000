abba1ed74460c6835818aae8ddc1df05d52e1778
crishanks12@gmail.com <crishanks12@gmail.com>
2018-07-23 23:16:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 var name = "Joe"
 var height = "74"
 
-if (name === "Joe"){
-  console.log("The name is correct")
-}
-else{
-  console.log("The height is correct")
-}
-
 // Don't worry about this
 module.exports = { name, height
 }
